41150580bb29feaf50fba2a4b42826e24feabbe6
MuhdAdil13 <adilrahim333@gmail.com>
2024-07-15 12:57:53 +0530

    Models Created 15/07/24

--- backend-django/SmartNexa/SmartNexa/settings.py
+++ backend-django/SmartNexa/SmartNexa/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'SmartNexa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'db_smartnexa',
+        'USER':'user_smartnexa',
+        'PASSWORD':'smartnexa',
+        'HOST':'localhost',
+        'PORT':'3306'
+
     }
 }
 
--- backend-django/SmartNexa/base/models.py
+++ backend-django/SmartNexa/base/models.py
@@ -9,5 +9,41 @@ class customer(models.Model):
     gender=models.CharField(max_length=1)
     address_1=models.TextField()
     address_2=models.TextField()
-    cart_id=models.IntegerField()
+
+class admin(models.Model):
+    admin_id=models.AutoField(primary_key=True)
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    role=models.CharField(max_length=255)
+    employee_id=models.IntegerField()
+
+class seller(models.Model):
+    seller_id=models.AutoField(primary_key=True)
+    seller_name=models.CharField(max_length=255)
+    legal_name=models.CharField(max_length=255)
+    contact_person=models.CharField(max_length=255)
+    alter_mobile=models.CharField(max_length=14)
+
+class order(models.Model):
+    order_id=models.AutoField(primary_key=True)
+    customer_id=models.IntegerField()
+    order_date=models.DateTimeField()
+    order_status=models.CharField(max_length=10)
+    transaction_id=models.IntegerField()
+
+class order_details(models.Model):
+    order_details_id=models.AutoField(primary_key=True)
+    order_id=models.IntegerField()
+    product_id=models.IntegerField()
+    product_quantity=models.IntegerField()
+    total_amount=models.DecimalField(7,2)
+
+class cart(models.Model):
+    product_id=models.IntegerField()
+    customer_id=models.IntegerField()
+    product_quantity=models.IntegerField()
+
+    class Meta:
+        unique_together = ('customer_id', 'product_id')
+
 
