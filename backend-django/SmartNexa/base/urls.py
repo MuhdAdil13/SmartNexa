b945e361ed0172f8112b6e9b8c61b56839a3af95
MuhdAdil13 <adilrahim333@gmail.com>
2024-08-06 13:00:03 +0530

    API Created for models(customer,admin)

--- backend-django/SmartNexa/base/urls.py
+++ backend-django/SmartNexa/base/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path,include
 from base import views
+from .views import CustomerViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet)
 
 urlpatterns = [
-    path('home',views.display)
+    path('', include(router.urls))
 ]
+
