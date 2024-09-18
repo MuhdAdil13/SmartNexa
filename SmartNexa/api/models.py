51d63426527ba79ad205f96d4b4733840a84c208
MuhdAdil13 <adilrahim333@gmail.com>
2024-09-18 13:11:09 +0530

    Creating user registration

---
+++ SmartNexa/api/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
+
+class User(AbstractBaseUser):
+    user_id = models.AutoField(primary_key=True)  # Auto-incrementing ID
+    username = models.CharField(max_length=100, unique=True)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)  # Ensure email is unique
+    mobile = models.CharField(max_length=15, blank=True, null=True)  # Optional mobile field
+    is_active = models.BooleanField(True)
+    is_superuser = models.BooleanField(False)
+    objects = SmartNexaUserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['firstname','lastname','mobile']
+    
+    def __str__(self):
+        return self.username
+    
+
+class SmartNexaUserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError("User Must have an Email Address")
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault('is_superuser',True)
+        return self.create_user(email,password,**extra_fields)
+
+
